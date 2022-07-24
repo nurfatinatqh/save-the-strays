1630404609ecc30e8ff8ea9c4646b5268faf31bc
Fatin Atiqah <nurfatinatiqahrozadin@gmail.com>
2022-07-24 21:42:39 +0800

    Update Change

--- resources/views/view-donors-page.blade.php
+++ resources/views/view-donors-page.blade.php
@@ -9,7 +9,7 @@
   </script>
   <div>
     <section class="module-tips" id="alt-features">
-        <div class="container"><br><br>
+        <div class="container" id="here"> <br><br>
           <div class="row">
             <div class="">
               <h2 class="module-title font-alt">SHARE THE LOVE BY DONATING</h2>
@@ -96,64 +96,121 @@
                     <br>
                 </div>
             </div><br><br>
-            @if ($donation->updated_condition != null)
-                <div style="text-align: center"><hr><b>CASE OUTCOME</b><hr><br></div>
-                <div class="row">
-                <div class="col-sm-3"></div>
-                    <div style="padding: 10px; height:350px; text-align:right;" class="col-sm-3">
-                        @php
-                        $url = Storage::disk('s3')->temporaryUrl(
-                            $donation->updated_condition,
-                            now()->addMinutes(10)
-                        );
-                        @endphp
-                        <img style="max-width: 100%; max-height:100%" src="{{$url}}" />
-                        {{-- <img style="height: auto" src="{{ url($donation->updated_condition) }}" /> --}}
-                    </div>
-                    <div style="padding: 10px; height:350px; text-align:left;" class="col-sm-3">
-                        @php
-                        $url = Storage::disk('s3')->temporaryUrl(
-                            $donation->receipt,
-                            now()->addMinutes(10)
-                        );
-                        @endphp
-                        <img style="max-width: 100%; max-height:100%; border: 1px solid beige;" src="{{$url}}" />
-                        {{-- <img style="height: auto" src="{{ url($donation->receipt) }}" /> --}}
-                    </div>
-                    <div class="col-sm-3"></div>
-                </div>
-            @endif
-          
-          <br><br>
-          <div id="here">
-            @if (count($donation->donors) == 0)
-                <hr>
-                <div class="align-center">No Donor Yet</div>
-                <hr>
-            @else
-                <br>
+            <div style="text-align: center; background-color:rgb(48, 205, 211); color:white;"><hr><b>CASE OUTCOME</b><hr></div><br>
+            <br><br>
+            @if ($donation->updated_condition_1 != null)
                 <table style="width: 100%">
-                    <tr style="border: 1px solid;  padding:10px; background-color:coral; color:white">
-                        <th>&nbsp;<br><br></th>
-                        <th> # </th>
-                        <th> NAME </th>
-                        <th> AMOUNT OF DONATION </th>
-                        <th> DATE </th>
+                    <tr style="border: 1px solid #eee;  padding:10px; color:black">
+                        <th style="width: 5%; padding: 20px;"> # </th>
+                        <th style="width: 30%; padding: 20px;"> CURRENT CONDITION </th>
+                        <th style="width: 20%; padding: 20px;"> RECEIPT </th>
+                        <th style="width: 30%; padding: 20px;"> DESCRIPTION </th>
                     </tr>
-                    
-                    @foreach ($donation->donors as $key => $donor)
-                    <tr style="border: 1px solid #eee; padding:10px">
-                        <td>&nbsp;<br><br></td>
-                        <td> {{$key + 1}} </td>
-                        <td> {{$donor->name}} </td>
-                        <td> RM{{number_format((float)$donor->amount_of_donation, 2, '.', '')}}</td>
-                        <td> {{Carbon\Carbon::parse($donor->created_at)->format('d/m/Y')}} </td>
-                    </tr>
-                    @endforeach
+                    @if ($donation->updated_condition_1 != null)
+                        <tr style="border: 1px solid #eee; padding:10px;">
+                            <td style="padding: 20px;"> 1 </td>
+                            <td style="padding: 20px;"> 
+                                @php
+                                $url = Storage::disk('s3')->temporaryUrl(
+                                    $donation->updated_condition_1,
+                                    now()->addMinutes(10)
+                                );
+                                @endphp
+                                <img style="max-width: 100%;" src="{{$url}}" />
+                            </td>
+                            <td style="padding: 20px;">
+                                @php
+                                $url = Storage::disk('s3')->temporaryUrl(
+                                    $donation->receipt_1,
+                                    now()->addMinutes(10)
+                                );
+                                @endphp
+                                <img style="max-width: 100%; border: 1px solid beige;" src="{{$url}}" />
+                            </td>
+                            <td style="padding: 20px;"> {{$donation->description_1}} </td>
+                        </tr>
+                    @endif
+                    @if ($donation->updated_condition_2 != null)
+                        <tr style="border: 1px solid #eee; padding:10px;">
+                            <td style="padding: 20px;"> 2 </td>
+                            <td style="padding: 20px;"> 
+                                @php
+                                $url = Storage::disk('s3')->temporaryUrl(
+                                    $donation->updated_condition_2,
+                                    now()->addMinutes(10)
+                                );
+                                @endphp
+                                <img style="max-width: 100%;" src="{{$url}}" />
+                            </td>
+                            <td style="padding: 20px;">
+                                @php
+                                $url = Storage::disk('s3')->temporaryUrl(
+                                    $donation->receipt_2,
+                                    now()->addMinutes(10)
+                                );
+                                @endphp
+                                <img style="max-width: 100%; border: 1px solid beige;" src="{{$url}}" />
+                            </td>
+                            <td style="padding: 20px;"> {{$donation->description_2}} </td>
+                        </tr>
+                    @endif
+                    @if ($donation->updated_condition_3 != null)
+                        <tr style="border: 1px solid #eee; padding:10px;">
+                            <td style="padding: 20px;"> 3 </td>
+                            <td style="padding: 20px;"> 
+                                @php
+                                $url = Storage::disk('s3')->temporaryUrl(
+                                    $donation->updated_condition_3,
+                                    now()->addMinutes(10)
+                                );
+                                @endphp
+                                <img style="max-width: 100%;" src="{{$url}}" />
+                            </td>
+                            <td style="padding: 20px;">
+                                @php
+                                $url = Storage::disk('s3')->temporaryUrl(
+                                    $donation->receipt_3,
+                                    now()->addMinutes(10)
+                                );
+                                @endphp
+                                <img style="max-width: 100%; border: 1px solid beige;" src="{{$url}}" />
+                            </td>
+                            <td style="padding: 20px;"> {{$donation->description_3}} </td>
+                        </tr>
+                    @endif
                     
                 </table> 
             @endif
-          </div>
+          
+          <br><br>
+          <div style="text-align: center; background-color:coral; color:white;"><hr><b>DONOR LIST</b><hr></div><br>
+          <br><br>
+          @if (count($donation->donors) == 0)
+            <hr>
+            <div class="align-center">No Donor Yet</div>
+            <hr>
+          @else
+            <table style="width: 100%">
+                <tr style="border: 1px solid #eee;  padding:10px; color:black">
+                    <th>&nbsp;<br><br></th>
+                    <th> # </th>
+                    <th> NAME </th>
+                    <th> AMOUNT OF DONATION </th>
+                    <th> DATE </th>
+                </tr>
+                
+                @foreach ($donation->donors as $key => $donor)
+                <tr style="border: 1px solid #eee; padding:10px">
+                    <td>&nbsp;<br><br></td>
+                    <td> {{$key + 1}} </td>
+                    <td> {{$donor->name}} </td>
+                    <td> RM{{number_format((float)$donor->amount_of_donation, 2, '.', '')}}</td>
+                    <td> {{Carbon\Carbon::parse($donor->created_at)->format('d/m/Y')}} </td>
+                </tr>
+                @endforeach
+                
+            </table> 
+          @endif
           
         </div>
       </section>
